36489a748733f47d3396b1c0b5ca34fde0f8ec28
Diego Carrion <dc.rec1@gmail.com>
2011-06-02 15:36:04 -0300

    refactors

--- lib/redtube/video.rb
+++ lib/redtube/video.rb
@@ -1,25 +1,32 @@
 module Redtube
   class Video
-    attr_accessor :id, :duration, :views, :id, :rating, :ratings, :title, :url, :thumb
+    attr_accessor :id, :duration, :views, :rating, :ratings, :title, :url, :thumb
 
     def self.find(id)
-      video = get("data=redtube.Videos.getVideoById&video_id=#{id}").first
-      new.tap do |object|
-        object.id = video["video_id"]
-        %w(duration views rating ratings title url thumb).each { |name| object.send "#{name}=", video[name] }
-      end
+      find_by_query("getVideoById&video_id=#{id}").first
     end
 
     def self.search(term)
-      get("data=redtube.Videos.searchVideos&search=#{term}").map do |video|
-        new
-      end
+      find_by_query "searchVideos&search=#{term}"
+    end
+
+    def initialize(video)
+      @id = video["video_id"]
+      %w(duration views rating ratings title url thumb).each { |name| send "#{name}=", video[name] }
     end
 
     private
 
+    def self.find_by_query(query)
+      videos_by_query(query).map { |video| new video }
+    end
+
+    def videos_by_query(query)
+      Nokogiri::XML(get(query)).css "video"
+    end
+
     def self.get(query)
-      Nokogiri::XML(open("http://api.redtube.com/?output=xml&#{query}").read).css("video")
+      open("http://api.redtube.com/?output=xml&data=redtube.Videos.#{query}").read
     end
   end
 end
