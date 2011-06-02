0552b4b366ec747e4ac904aa9bf21bb2da743652
Diego Carrion <dc.rec1@gmail.com>
2011-06-02 01:32:11 -0300

    refactors

--- lib/redtube/video.rb
+++ lib/redtube/video.rb
@@ -1,15 +1,19 @@
 module Redtube
   class Video
-    attr_reader :duration, :views, :id, :rating, :ratings, :title, :url, :thumb
+    attr_accessor :id, :duration, :views, :id, :rating, :ratings, :title, :url, :thumb
 
     def self.find(id)
-      video = Nokogiri::XML(Typhoeus::Request.get("http://api.redtube.com/?data=redtube.Videos.getVideoById&video_id=#{id}&output=xml").body).css("video").first
+      video = get "data=redtube.Videos.getVideoById&video_id=#{id}"
       new.tap do |object|
-        object.instance_variable_set "@id", video["video_id"]
-        %w(duration views rating ratings title url thumb).each do |name|
-          object.instance_variable_set "@#{name}", video[name]
-        end
+        object.id = video["video_id"]
+        %w(duration views rating ratings title url thumb).each { |name| object.send "#{name}=", video[name] }
       end
+    end
+
+    private
+
+    def self.get(query)
+      Nokogiri::XML(Typhoeus::Request.get("http://api.redtube.com/?output=xml&#{query}").body).css("video").first
     end
   end
 end
